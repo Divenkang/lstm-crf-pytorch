15d67b15616bfa00cebbd0ad5a9a369f06141a5b
threelittlemonkeys <sjlee7642@gmail.com>
2020-07-01 13:40:32 +0900

    Update by commit

--- parameters.py
+++ parameters.py
@@ -3,12 +3,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-UNIT = "word" # unit of tokenization (char, word, sent)
+UNIT = "sent" # unit of tokenization (char, word, sent)
 TASK = None # task (None, word-segmentation, sentence-segmentation)
 RNN_TYPE = "LSTM" # LSTM or GRU
 NUM_DIRS = 2 # unidirectional: 1, bidirectional: 2
 NUM_LAYERS = 2
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 HRE = (UNIT == "sent") # hierarchical recurrent encoding
 EMBED = {"lookup": 300} # embeddings (char-cnn, char-rnn, lookup, sae)
 EMBED_SIZE = sum(EMBED.values())
@@ -18,15 +18,10 @@ LEARNING_RATE = 2e-4
 EVAL_EVERY = 10
 SAVE_EVERY = 10
 
-PAD = "<PAD>" # padding
-SOS = "<SOS>" # start of sequence
-EOS = "<EOS>" # end of sequence
-UNK = "<UNK>" # unknown token
-
-PAD_IDX = 0
-SOS_IDX = 1
-EOS_IDX = 2
-UNK_IDX = 3
+PAD, PAD_IDX = "<PAD>", 0 # padding
+SOS, SOS_IDX = "<SOS>", 1 # start of sequence
+EOS, EOS_IDX = "<EOS>", 2 # end of sequence
+UNK, UNK_IDX = "<UNK>", 3 # unknown token
 
 CUDA = torch.cuda.is_available()
 torch.manual_seed(0) # for reproducibility
